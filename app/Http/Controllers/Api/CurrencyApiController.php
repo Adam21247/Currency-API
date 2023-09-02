9ccb0f041e7656ef89ccda2591622eded277a522
AdamOrzech <adamorzech92@gmail.com>
2023-09-02 18:27:53 +0200

    I corrected fetchAndStoreCurrencyExchange function

--- app/Http/Controllers/Api/CurrencyApiController.php
+++ app/Http/Controllers/Api/CurrencyApiController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Models\CurrencyExchange;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
-use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Cache;
 
@@ -19,10 +18,8 @@ class CurrencyApiController extends Controller
     {
         $response = Http::get('http://api.nbp.pl/api/exchangerates/tables/A/');
         $currencies = $response->json();
-
-        $selectedCurrencies = ['USD', 'EUR', 'GBP'];
+        $selectedCurrencies = ['EUR', 'GBP', 'USD'];
         $selectedExchangeRates = [];
-
         foreach ($currencies[0]['rates'] as $rate) {
             if (in_array($rate['code'], $selectedCurrencies)) {
                 $selectedExchangeRates[] = [
@@ -37,65 +34,26 @@ class CurrencyApiController extends Controller
     public function fetchAndStoreCurrencyExchange()
     {
         $currencies = ['EUR', 'GBP', 'USD'];
-
+        $messages = [];
         foreach ($currencies as $currency) {
             $lastUpdated = Cache::get("last_update:{$currency}");
+
             if ($lastUpdated && Carbon::now()->isSameDay($lastUpdated)) {
-                return response()->json(['message' => 'Currency exchange rates already been fetched today.']);
+                $messages[$currency] = "Data for {$currency} has already been updated today.";
             } else {
-                $response = Http::get("http://api.nbp.pl/api/exchangerates/rates/a/{$currency}/last/2/");
+                $response = Http::get("http://api.nbp.pl/api/exchangerates/rates/a/{$currency}/last/7/");
                 $data = $response->json();
-
-
                 foreach ($data['rates'] as $rateData) {
-
                     CurrencyExchange::create([
                         'currency_code' => $currency,
                         'exchange_rate' => $rateData['mid'],
                         'date' => $rateData['effectiveDate'],
                     ]);
-
                 }
                 Cache::put("last_update:{$currency}", Carbon::now(), now()->addDay());
+                $messages[$currency] = "Currency exchange rates for {$currency} fetched and stored.";
             }
-
-
-
         }
-        return response()->json(['message' => 'Currency exchange rates fetched and stored.']);
-    }
-
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-
-
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        return response()->json(['messages' => $messages]);
     }
 }
