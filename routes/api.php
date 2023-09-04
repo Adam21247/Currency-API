2f2d4fb00da766558b8121b6ee41636f6f3821ed
AdamOrzech <adamorzech92@gmail.com>
2023-09-04 12:29:35 +0200

    I just corrected AuthController. Mistake was in return in line 31

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 
 
 use App\Http\Controllers\Api\CurrencyApiController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,10 +18,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+Route::post('register',[AuthController::class, 'register']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('currency-rates',[CurrencyApiController::class, 'index']);
+    Route::get('fetch-currencies', [CurrencyApiController::class, 'fetchAndStoreCurrencyExchange']);
+    Route::get('currency-rates/{date}',[CurrencyApiController::class, 'getCurrencyRatesByDate']);
 });
 
-Route::get('currency-rates',[CurrencyApiController::class, 'index']);
-Route::get('fetch-currencies', [CurrencyApiController::class, 'fetchAndStoreCurrencyExchange']);
-Route::get('currency-rates/{date}',[CurrencyApiController::class, 'getCurrencyRatesByDate']);
+
+
